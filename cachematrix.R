afd51c6db38c700ec8e1a85705d2fcb22ad2dbfa
irving <irvingthemagnificent@yahoo.com>
2015-04-23 14:38:02 -0400

    added soem header info

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
-## Homework assignemnt
+## Programming Assignment 2 for Coursera R programming course
+## Dave Vandervort;  4/23/2015
+## The code herein was adapted from code supplied to describe the assignment
+## Original found at https://class.coursera.org/rprog-013/human_grading/view/courses/973494/assessments/3/submissions
 
-## Make a matrix that can cache its inverse
+## Make a vector that caches the inverse of the supplied matrix
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -18,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-# Return cached solution or new solution to inverse of a square matrix
+# Return cached or new solution to inverse of a square matrix
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     m <- x$getinverse()
